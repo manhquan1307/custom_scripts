1f9b2f92ef1e34420e5d5a95499b9fe3fd901c10
))))) <92537951+manhquan1307@users.noreply.github.com>
2025-05-29 10:48:26 +0700

    overrides 403 error

--- custom_scripts/__init__.py
+++ custom_scripts/__init__.py
@@ -2,6 +2,8 @@ __version__ = "0.0.1"
 
 import frappe
 from custom_scripts.overrides import overrides_403_err
+import frappe.exceptions
 
+frappe.exceptions.PermissionError.http_status_code = 404
 frappe.permissions.has_permission = overrides_403_err.custom_has_permission
 frappe.has_permission = overrides_403_err.custom_init_has_permission
